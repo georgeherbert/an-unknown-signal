7105dc9ec0a6bb292dc405c71cb5da567f353768
George Herbert <george.herbert@icloud.com>
2021-03-11 17:56:17 +0000

    Tidied up code that checks if the arguments are valid and --plot argument now displays points

--- lsr.py
+++ lsr.py
@@ -1,19 +1,25 @@
 import sys
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Plots the data
+def plot(data):
+    fix, ax = plt.subplots()
+    ax.scatter(data[:,0], data[:,1])
+    plt.show()
+
+# Main function
 def main():
     data = np.genfromtxt(sys.argv[1], delimiter = ',')
-    print(data)
-
-def checkArgs():
-    valid = False
-    if (len(sys.argv) == 2) | (len(sys.argv) == 3):
-        valid = True
-    return valid
+    
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "--plot":    
+            plot(data)
 
 if __name__ == "__main__":
-    if checkArgs() == True:
+    numOfArgs = len(sys.argv)
+    if ((numOfArgs == 2) | (numOfArgs == 3)):
         main()
     else:
         print("Invalid argument(s)")

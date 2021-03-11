41351b3ae0170750e9b2776b054e7fc887e57f69
George Herbert <george.herbert@icloud.com>
2021-03-11 17:44:24 +0000

    Data from files is now converted to np.array()

--- lsr.py
+++ lsr.py
@@ -1,14 +1,16 @@
 import sys
+import csv
 import numpy as np
+
+def main():
+    data = np.genfromtxt(sys.argv[1], delimiter = ',')
+    print(data)
 
 def checkArgs():
     valid = False
     if (len(sys.argv) == 2) | (len(sys.argv) == 3):
         valid = True
     return valid
-
-def main():
-    print(sys.argv[1])
 
 if __name__ == "__main__":
     if checkArgs() == True:
